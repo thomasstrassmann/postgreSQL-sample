acf4c7ac3ef0bb89dc44915c3a0591f841f451e2
thomasstrassmann <thomas_strassmann@freenet.de>
2022-11-09 10:46:13 +0000

    bug fix: wrong init of db

--- Chinook_PostgreSql.sql
+++ Chinook_PostgreSql.sql
@@ -15815,4 +15815,3 @@ INSERT INTO "PlaylistTrack" ("PlaylistId", "TrackId") VALUES (17, 2096);
 INSERT INTO "PlaylistTrack" ("PlaylistId", "TrackId") VALUES (17, 3290);
 INSERT INTO "PlaylistTrack" ("PlaylistId", "TrackId") VALUES (18, 597);
 
-
